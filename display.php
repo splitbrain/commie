78970cb1f807bcae36a30c411a10a35fc341a229
Josh Martin <jtmartin@G1WSC3-G249.jomax.paholdings.com>
2016-01-06 11:41:55 -0700

    added new futures and style
    new layout, and started adding list of pastes for private use

---
+++ display.php
@@ -0,0 +1,359 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="lib/style.css" rel="stylesheet"/>
+    <link href="lib/favicon.ico" rel="shortcut icon"/>
+    <title>Commie Paste</title>
+</head>
+<body>
+
+<div id="header">
+<div id="header_content">
+<h1>Commie Paste</h1>
+commie is a pastebin script with line commenting support.
+</div>
+</div>
+<br><br>
+<div id="content">
+<?PHP
+if ($handle = opendir('data/a')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/b')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/c')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/d')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/e')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/f')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/g')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/h')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/i')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/j')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/k')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/l')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/m')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/n')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/o')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/p')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/q')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/r')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/s')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/t')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/u')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/v')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/w')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/x')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/y')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+
+if ($handle = opendir('data/z')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry".'<br>';
+        }
+    }
+
+    closedir($handle);
+}
+?>
+</div>
+</body>
+</html>
